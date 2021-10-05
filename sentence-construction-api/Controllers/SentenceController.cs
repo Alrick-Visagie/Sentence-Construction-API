7025d3265b0a6187b7e04c14f0d39170f8de9dba
Alrick Visagie <alrick.visagie@motuscr.co.za>
2021-10-05 19:39:54 +0200

    Initial Commit

---
+++ sentence-construction-api/Controllers/SentenceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SentenceConstruction.Models;
+using SentenceConstruction.Serices;
+using System.Collections.Generic;
+
+namespace SentenceConstruction.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SentenceController : ControllerBase
+    {
+        private readonly ISentenceService _sentenceService;
+        public SentenceController(ISentenceService sentenceService)
+        {
+            _sentenceService = sentenceService;
+        }
+
+        [HttpGet("get-sentences")]
+        public List<SentenceDto> GetSentences()
+        {
+            return _sentenceService.GetSentences();
+        }
+
+        [HttpPost("save-sentence")]
+        public void SaveSentence(SentenceDto sentence)
+        {
+             _sentenceService.SaveSentence(sentence);
+        }
+        
+        [HttpDelete("delete-sentence")]
+        public void DeleteSentence([FromQuery]string sentenceId)
+        {
+            _sentenceService.DeleteSentence(sentenceId);
+        }
+    }
+}
---
+++ sentence-construction-api/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SentenceConstruction.Repository;
+using SentenceConstruction.Serices;
+
+namespace sentence_construction_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<SentenceConstructionDbContext>(db => db.UseSqlServer(Configuration.GetConnectionString("Database")));
+            services.AddScoped<ISentenceService, SentenceService>();
+            services.AddScoped<IWordService, WordService>();
+            services.AddScoped<ISentenceRepository, SentenceRepository>();
+            services.AddScoped<IWordRepository, WordRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
